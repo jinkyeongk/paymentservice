0896ab058d4e7464bf8eecdc60667d7db61db12a
JinKyeong Kim <jinkyeongk@naver.com>
2024-09-27 17:01:35 +0900

    결제 시스템 프로젝트

---
+++ paymentservice/src/main/kotlin/com/example/paymentservice/payment/adapter/in/web/view/PaymentController.kt
@@ -0,0 +1,28 @@
+package com.example.paymentservice.payment.adapter.`in`.web.view
+
+import com.example.paymentservice.common.WebAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import reactor.core.publisher.Mono
+
+
+@Controller
+@WebAdapter //웹 요청을 받아서 웹어플리케이션에 명령을 전달하는 웹어댑터 어노테이션
+@RequestMapping("/v1/toss") //결제 성공과 실패에 대한 리다이렉션 렌더링 코드
+class PaymentController {
+
+
+    @GetMapping("/success")
+    fun successPage(): Mono<String>
+    {
+        return Mono.just("success")
+    }
+
+    @GetMapping("/fail")
+    fun failPage(): Mono<String>
+    {
+        return Mono.just("fail")
+    }
+}
